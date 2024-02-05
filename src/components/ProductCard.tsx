a00585051ba58bc15af424661806dd1dc56d84bd
joshtin2505 <you@example.com>
2024-02-05 12:16:05 -0500

    Refactor ProductCard component styles

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -29,11 +29,11 @@ function ProductCard({product, cartIsOpen}: {product: Product, cartIsOpen: boole
     }, [inCart, id])
 
     return (
-        <div className="w-60 h-auto shadow">
-            <figure className="w-full  ">
+        <div className="productCardContainer ">
+            <figure className="w-full">
                 <Image className="productImage" src={imgSrc} alt={title} width={240} height={240}/>
             </figure>
-            <section className="w-full p-4 py-2 rounded-bl-md rounded-br-rounded-bl-md">
+            <section className="productSectContainer">
                 <h5 className="text-lg font-medium">{title}</h5>
                 <span>${price}</span>
                 <p>{description}</p>
@@ -41,22 +41,22 @@ function ProductCard({product, cartIsOpen}: {product: Product, cartIsOpen: boole
                 <h6 className="font-medium">Tags</h6>
                 <div className="mb-8 flex gap-1 flex-wrap">
                     {
-                        tags.slice(0,5).map((tag) => <button className="tag p-2 py-1 rounded-md capitalize transition-all hover:shadow-md" key={tag}>{tag}</button>)
+                        tags.slice(0,5).map((tag) => <button className="tag" key={tag}>{tag}</button>)
                     }
                 </div>
-                <footer className="w-full flex justify-center items-center relative">
+                <footer className="">
                     {
                         isClient && productInCart ? 
-                        <div className="flex w-full absolute bottom-[-24px] gap-3 justify-center items-center">
+                        <div className="inCartBtnCont">
                             <button 
                             onClick={handleClickRemoveFromCart}
-                            className="btnRemoveFromCart p-2 rounded-md  transition-all hover:scale-105 hover:shadow-xl active:scale-100 flex gap-1 items-center text-center">
+                            className="btnRemoveFromCart ">
                                 Quitar
                                 <BsCartX size={20}/>
                             </button>
                             <button
                             onClick={() => setOpenCart(true)}
-                            className="btnGoToCart p-2 rounded-md transition-all hover:scale-105 hover:shadow-xl active:scale-100 flex gap-1 items-center text-center disabled:opacity-60 disabled:hover:scale-100 disabled:hover:shadow-md disabled:active:scale-100 disabled:cursor-not-allowed"
+                            className="btnGoToCart "
                             disabled={cartIsOpen}
                             >
                                 Ir
@@ -66,7 +66,7 @@ function ProductCard({product, cartIsOpen}: {product: Product, cartIsOpen: boole
                         : 
                         <button 
                         onClick={handleClickToCart}
-                        className="btnAddToCart p-2 rounded-md absolute bottom-[-24px] transition-all hover:scale-105 hover:shadow-xl active:scale-100 flex gap-1 items-center text-center">
+                        className="btnAddToCart">
                         Añadir al carrito 
                         <BsCart size={20}/>
                         </button>
