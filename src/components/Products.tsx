05457d1689c1c10dc89bdf1b1457e84e1237d635
joshtin2505 <you@example.com>
2024-02-05 13:25:19 -0500

    Refactor Products component Styles

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -5,11 +5,11 @@ import './Products.css'
 function Products() {
   const {openCart} = useCart()
   return (
-    <section className="w-full flex justify-center items-center flex-col gap-6 productsSection overflow-y-scroll lg:px-28 px-8 py-28 relative">
-      <header className='flex justify-center items-center w-full absolute top-0 py-5'>
-        <h2 className="text-5xl font-semibold txt-deg-primary drop-shadow-md  " id="products">Productos</h2>
+    <section className="productsSection">
+      <header className='productsSecHeader'>
+        <h2 className="txt-deg-primary" id="products">Productos</h2>
       </header>
-      <div className="flex flex-wrap justify-center items-center gap-8 productsContainer">
+      <div className="productsContainer">
         {
             ProductList && ProductList.map((item) => {
               return <ProductCard product={item} cartIsOpen={openCart} key={item.id}/>
