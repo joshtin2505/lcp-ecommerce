13fd3f27332eba99eb45c1acf141602297a064f4
joshtin2505 <you@example.com>
2024-02-06 14:51:41 -0500

    Actualizar estilos globales y secciones de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,21 @@
 'use client'
 import Nav from "@/components/Nav"
-import ImageGallery from "@/components/ImageGallery"
-import useTheme from "@/hooks/useThemes"
-import WelcomText from "@/components/WelcomText"
-import AboutUs from "@/components/AboutUs"
-import Products from "@/components/Products"
-import Footer from "@/components/Footer"
-import CartContextProvider from "@/context/CartContext";
 import Cart from "@/components/Cart"
+import WelcomeSection from "@/components/WelcomeSection"
+import Products from "@/components/Products"
+import AboutUs from "@/components/AboutUs"
+import Footer from "@/components/Footer"
+import CartContextProvider from "@/context/CartContext"
 
 export default function Home() {
-  const setTheme = useTheme()
 
   return (
     <CartContextProvider>
-      <header className="w-screen relative px-14 pt-4">
-          <Nav/>
+      <header className="home-header">
+          <Nav/> 
       </header>      
-      <main className="main-container flex min-h-screen flex-col items-center justify-between relative ">
-        <section className="welcome-section w-full items-center flex justify-center md:px-20 flex-col md:flex-row md:h-screen shadow-md z-10">
-          <WelcomText/>
-          <ImageGallery/>
-        </section>
+      <main className="main-container">
+        <WelcomeSection/>
         <Products/>
         <AboutUs/>
       </main>
