d7ca67267579853e555ccacc66dd6e8f06c4cc73
joshtin2505 <you@example.com>
2024-02-22 16:43:08 -0500

    Agregado archivo .eslintrc.js y eliminado archivo .eslintrc.json

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+export default {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "next/core-web-vitals",
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
