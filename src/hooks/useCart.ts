c628e2c2bb08f8887dd7780b5b903f902785c6a3
joshtin2505 <you@example.com>
2024-02-22 16:19:56 -0500

    Añadida función inCart a useCart.ts

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -2,16 +2,14 @@ import { CartContext } from "@/context/CartContext"
 import { useContext } from "react"
 
 export const useCart = () => {
-
-
-    const context = useContext(CartContext)
-    if (context == undefined){
-      throw new Error("useCart must be used within a CartProvider")
-    }
-    const {cart} = context
-    function inCart(id: number){
-        return cart.some((item) => item.id === id) // retorna el indice del producto en el carrito
-    }
-    
-    return {...context, inCart}
+  const context = useContext(CartContext)
+  if (context == undefined) {
+    throw new Error("useCart must be used within a CartProvider")
   }
+  const { cart } = context
+  function inCart(id: number) {
+    return cart.some((item) => item.id === id) // retorna el indice del producto en el carrito
+  }
+
+  return { ...context, inCart }
+}
