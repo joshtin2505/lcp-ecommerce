2f13cc9b193ba11a0652ff6dbffe71bd5ed20302
Joshtin <91860458+joshtin2505@users.noreply.github.com>
2024-03-06 01:12:45 +0000

    Add signup layout and page components

---
+++ src/app/signup/layout.tsx
@@ -0,0 +1,5 @@
+function layout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>
+}
+
+export default layout
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,5 @@
+function page() {
+  return <div className="">page</div>
+}
+
+export default page
