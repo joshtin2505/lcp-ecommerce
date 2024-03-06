b8c90a31d735d73091d82f752561abb6f68e132e
joshtin2505 <justincastro2505@gmail.com>
2024-03-06 10:06:53 -0500

    Refactor signup page to use SignUpForm component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,29 +1,10 @@
+import SignUpForm from "@/components/signup/SignUpForm"
 import { Button } from "@/components/ui/button"
 
 function page() {
   return (
     <main className="flex flex-col justify-center items-center">
-      <form action="" className="flex flex-col max-w-80">
-        <h1>Sign Up</h1>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input type="password" id="confirmPassword" />
-        </div>
-        <Button
-          type="submit"
-          className="bg-primary-500 hover:bg-primary-300 max-w-28 "
-        >
-          Sign Up
-        </Button>
-      </form>
+      <SignUpForm />
     </main>
   )
 }
