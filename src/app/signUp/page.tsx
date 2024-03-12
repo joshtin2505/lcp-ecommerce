561774f00950333ac71f10549fd6bce66a63e428
Joshtin <91860458+joshtin2505@users.noreply.github.com>
2024-03-12 00:33:31 +0000

    Update name folder

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,6 +1,4 @@
 import SignUpForm from "@/components/signup/SignUpForm"
-import { Button } from "@/components/ui/button"
-
 function page() {
   return (
     <main className="flex flex-col justify-center items-center welcome-section">
