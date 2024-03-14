5264035b4ec02eb7aacd538c97c588ae36b46958
joshtin2505 <justincastro2505@gmail.com>
2024-03-14 18:12:50 -0500

    Refactor user registration form schema

--- src/schemas/user.schemas.ts
+++ src/schemas/user.schemas.ts
@@ -1,10 +1,26 @@
 import { z } from "zod"
+const passwordSchema = z.string().min(8, {
+  message: "Password must be at least 8 characters long",
+})
 export const registerUserFormSchema = z.object({
-  name: z.string(),
-  lastName: z.string(),
-  email: z.string().email(),
-  password: z.string().min(8, {
-    message: "Password must be at least 8 characters long",
+  name: z
+    .string()
+    .min(3, {
+      message: "Name must be at least 3 characters long",
+    })
+    .max(200, { message: "Name must be less than 200 characters long" }),
+  lastName: z.string().min(3, {
+    message: "Last Name must be at least 3 characters long",
   }),
-  confirmPassword: z.string().min(8),
+  email: z.string().email({
+    message: "Please enter a valid email",
+  }),
+  password: passwordSchema,
+  confirmPassword: passwordSchema.refine(
+    (value, data) => {
+      // to fix
+      return data.password === value
+    },
+    { message: "Passwords do not match" }
+  ),
 })
