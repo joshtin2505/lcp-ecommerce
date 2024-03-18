bad914b9f93bd2a31010bf2dc1f75d46a6c2c14b
joshtin2505 <justincastro2505@gmail.com>
2024-03-17 22:30:51 -0500

    Call Login component

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,8 +1,8 @@
-import SignUpForm from "@/components/register/SignUpForm"
+import LoginForm from "@/components/login/LoginForm"
 function page() {
   return (
     <main className="flex flex-col justify-center items-center welcome-section">
-      <SignUpForm />
+      <LoginForm />
     </main>
   )
 }
