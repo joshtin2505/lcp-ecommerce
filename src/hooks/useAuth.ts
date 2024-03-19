5261eeb64077a75ce42c2934d66d0443d04db73d
joshtin2505 <justincastro2505@gmail.com>
2024-03-19 12:50:16 -0500

    Refactor useAuth hook and add Zustand store

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,10 +1,51 @@
-import { AuthContext } from "@/context/AuthContext"
-import { useContext } from "react"
+import { ReactNode, createContext, useState } from "react"
+import { create } from "zustand"
+import {
+  deleteUser,
+  getAll,
+  login,
+  logout,
+  registerOrdinal,
+  update,
+} from "@/api/users.api"
+import type { LoginUserForm, RegisterUserForm } from "@/types/zodExtended.types"
 
-export function useAuth() {
-  const context = useContext(AuthContext)
-  if (!context) {
-    throw new Error("useAuth must be used within an AuthProvider")
-  }
-  return context
+interface AuthState {
+  //   user: null | User
+  //   token: null | string
+  authResponse: null | Response
+  login: (data: LoginUserForm) => Promise<void>
+  logout: () => Promise<void>
+  register: (form: RegisterUserForm) => Promise<void>
+  //   update: (form: UpdateUserForm) => Promise<void>
+  deleteUser: (id: number) => Promise<void>
+  getAll: () => Promise<void>
 }
+
+export const useAuthStore = create<AuthState>((set) => ({
+  authResponse: null,
+  login: async (data) => {
+    const response = await login(data)
+    set({ authResponse: response })
+  },
+  logout: async () => {
+    const response = await logout()
+    set({ authResponse: response })
+  },
+  register: async (form) => {
+    const response = await registerOrdinal(form)
+    set({ authResponse: response })
+  },
+  deleteUser: async (id) => {
+    const response = await deleteUser(id)
+    set({ authResponse: response })
+  },
+  getAll: async () => {
+    const response = await getAll()
+    set({ authResponse: response })
+  },
+  //   update: async (form) => {
+  //     const response = await update(form)
+  //     set({ authResponse: response })
+  //   },
+}))
