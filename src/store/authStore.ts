ab33f696081d1dfac7d7cd88deb9949eee101bbf
joshtin2505 <justincastro2505@gmail.com>
2024-03-19 16:54:26 -0500

    Refactor authStore.ts: Update signInNormal function and authResponse type

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -9,49 +9,55 @@ import {
   update,
 } from "@/api/users.api"
 import type { LoginUserForm, RegisterUserForm } from "@/types/zodExtended.types"
+import { UserErrorsType } from "@/types/users.types"
 
 interface AuthState {
-  authResponse: null | Response | { message: string; status: number }
-  login: (data: LoginUserForm) => Promise<void>
-  logout: () => Promise<void>
-  register: (form: RegisterUserForm) => Promise<void>
-  //   update: (form: UpdateUserForm) => Promise<void>
-  deleteUser: (id: number) => Promise<void>
-  getAll: () => Promise<void>
+  authResponse: null | { message: UserErrorsType; status: 401 }
+  // | { message: string; status: Exclude<number, 401> }
+  // | Response
+  signInNormal: (data: LoginUserForm) => Promise<void>
+  // logout: () => Promise<void>
+  // register: (form: RegisterUserForm) => Promise<void>
+  // //   update: (form: UpdateUserForm) => Promise<void>
+  // deleteUser: (id: number) => Promise<void>
+  // getAll: () => Promise<void>
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   authResponse: null,
-  login: async (data) => {
+  signInNormal: async (data) => {
     try {
       const response = (await login(data)) as Response
-      console.log(response.status)
+      // console.log(await response.json())
+      const {message} = (await response.json()) as {
+        message: UserErrorsType
+      }
       set({
         authResponse: {
-          message: await response.json(),
-          status: response.status,
+          message,
+          status: response.status as 401,
         },
       })
     } catch (error) {
       console.error(error)
     }
   },
-  logout: async () => {
-    const response = await logout()
-    set({ authResponse: response })
-  },
-  register: async (form) => {
-    const response = await registerOrdinal(form)
-    set({ authResponse: response })
-  },
-  deleteUser: async (id) => {
-    const response = await deleteUser(id)
-    set({ authResponse: response })
-  },
-  getAll: async () => {
-    const response = await getAll()
-    set({ authResponse: response })
-  },
+  // logout: async () => {
+  //   const response = await logout()
+  //   set({ authResponse: response })
+  // },
+  // register: async (form) => {
+  //   const response = await registerOrdinal(form)
+  //   set({ authResponse: response })
+  // },
+  // deleteUser: async (id) => {
+  //   const response = await deleteUser(id)
+  //   set({ authResponse: response })
+  // },
+  // getAll: async () => {
+  //   const response = await getAll()
+  //   set({ authResponse: response })
+  // },
   //   update: async (form) => {
   //     const response = await update(form)
   //     set({ authResponse: response })
