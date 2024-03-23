9c84cb380d43b612620e4434110f6542b766f5c0
joshtin2505 <justincastro2505@gmail.com>
2024-03-23 15:11:45 -0500

    Add AxiosResponseApiType to extended.types.ts

--- src/types/extended.types.ts
+++ src/types/extended.types.ts
@@ -3,8 +3,12 @@ import {
   registerUserFormSchema,
   loginUserFormSchema,
 } from "@/schemas/user.schemas"
-
+import { AxiosResponse } from "axios"
+// Zod
 type RegisterUserForm = z.infer<typeof registerUserFormSchema>
 type LoginUserForm = z.infer<typeof loginUserFormSchema>
 // ☝️ infiero el tipo de el formSchema con z.infer
+
+// Axios
+type AxiosResponseApiType = AxiosResponse<any, any> // 👈 AxiosResponse
 export type { RegisterUserForm, LoginUserForm }
