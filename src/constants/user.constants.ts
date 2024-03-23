0e7bf781bc8a116429e58f77c8054a30cbc6855e
joshtin2505 <justincastro2505@gmail.com>
2024-03-23 13:31:56 -0500

    Refactor user constants and add UsersRoutes enum

--- src/constants/user.constants.ts
+++ src/constants/user.constants.ts
@@ -1,16 +1,16 @@
-export enum Roles {
+enum Roles {
   user = "user",
   masterAdmin = "masterAdmin",
   admin = "admin",
 }
-export enum UserErrors {
+enum UserErrors {
   EMAIL_NOT_FOUND = "EMAIL_NOT_FOUND",
   INCORRECT_PASSWORD = "INCORRECT_PASSWORD",
   USER_NOT_FOUND = "USER_NOT_FOUND",
   USER_ALREADY_EXISTS = "USER_ALREADY_EXISTS",
   USER_UNAUTHORIZED = "USER_UNAUTHORIZED",
 }
-export enum UserSuccess {
+enum UserSuccess {
   USER_CREATED = "USER_CREATED",
   USER_DELETED = "USER_DELETED",
   USER_UPDATED = "USER_UPDATED",
@@ -18,3 +18,16 @@ export enum UserSuccess {
   USER_LOGGED = "USER_LOGGED",
   USER_LOGOUT = "USER_LOGOUT",
 }
+
+enum UsersRoutes {
+  user = "/users",
+  all = "/users/all",
+  add = "/users/add",
+  update = "/users/update",
+  delete = "/users/delete",
+  login = "/users/login",
+  logout = "/users/logout",
+  addOrdinal = "/users/add-ordinal",
+}
+
+export { Roles, UserErrors, UserSuccess, UsersRoutes }
