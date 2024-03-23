70751925309747506860749f3ef8566a9fe9f2ae
joshtin2505 <justincastro2505@gmail.com>
2024-03-23 13:24:34 -0500

    Refactor config.ts to export additional constants

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,4 @@
-export const IS_DEV = process.env.NODE_ENV !== "production"
+const IS_DEV = process.env.NODE_ENV !== "production"
+const BACK_END_POINT = "https://lcp-backend-jl1j.onrender.com"
+
+export { BACK_END_POINT, IS_DEV }
