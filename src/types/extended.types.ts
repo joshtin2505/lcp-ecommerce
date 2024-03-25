83c575a0020d13620bd57113ac6e3ad1ed9648da
joshtin2505 <justincastro2505@gmail.com>
2024-03-25 17:29:44 -0500

    Update types and imports in extended.types.ts

--- src/types/extended.types.ts
+++ src/types/extended.types.ts
@@ -3,12 +3,13 @@ import {
   registerUserFormSchema,
   loginUserFormSchema,
 } from "@/schemas/user.schemas"
-import { AxiosResponse } from "axios"
+import type { AxiosResponse } from "axios"
+import type { LoginRejectType, LoginResolveType } from "./response.types"
 // Zod
 type RegisterUserForm = z.infer<typeof registerUserFormSchema>
 type LoginUserForm = z.infer<typeof loginUserFormSchema>
 // ☝️ infiero el tipo de el formSchema con z.infer
 
 // Axios
-type AxiosResponseApiType = AxiosResponse<any, any> // 👈 AxiosResponse
-export type { RegisterUserForm, LoginUserForm }
+type AxiosLoginResponse = AxiosResponse<LoginResolveType, LoginRejectType>
+export type { RegisterUserForm, LoginUserForm, AxiosLoginResponse }
