926e99868cb86b11c5765353a28037acf81ace3d
joshtin2505 <justincastro2505@gmail.com>
2024-03-25 17:31:44 -0500

    Add AuthProvider to Cover component

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -1,9 +1,14 @@
 "use client"
+import { AuthProvider } from "@/context/AuthContext"
 import CartContextProvider from "@/context/CartContext"
 import React from "react"
 
 function Cover({ children }: { children: React.ReactNode }) {
-  return <CartContextProvider>{children}</CartContextProvider>
+  return (
+    <CartContextProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </CartContextProvider>
+  )
 }
 
 export default Cover
